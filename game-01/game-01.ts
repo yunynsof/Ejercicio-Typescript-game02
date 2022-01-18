a18c597af1c76afd0b612881232a476ed641fb67
yunynsof <yunyrene@gmail.com>
2022-01-18 13:29:24 -0600

    Update game-01.ts
    Se optimizo el metodo de busqueda, ya que antes se buscaba posicion por posicion y recorria todo el arreglo; ahora se recorre posiciones iniciales por posiciones finales, y se valida si la suma de posiciones es menor a M, en caso de ser menor se rompe el flujo interno y se continua con el flujo externo.

--- game-01/game-01.ts
+++ game-01/game-01.ts
@@ -1,13 +1,14 @@
 //Metodo que recibe como parametros un arreglo de numeros enteros y un valor entero M; devuelve arreglo de los 2 numeros que sumados devuelven M
-obtainSubSet(array: number[], val:number){
-     for(let i=0;i<array.length;i++){
-	     for(let a=0;a<array.length;a++){
-		  if(i!=a){
-		  let sum = array[i]+array[a];
-		    if(sum==val)
-			return [array[i],array[a]];
-		  }
-		}
-	 }
-	return [0];
+obtainSubSet(newArray: number[], val:number){
+     newArray.sort(function(a, b){return a - b}); //A la cadena se le realiza un ordenamiento ascendente.
+	 
+	   for(let i=0;i<newArray.length;i++){ //Se realiza un for ascendente al arreglo
+		    for(let a=newArray.length;a>0;a--){ //Se realiza un for descendente al arreglo
+				if(newArray[i]+newArray[a]==val) // Si suma es igual a M finalizo todo el flujo
+					return [newArray[i],newArray[a]];
+				if(newArray[i]+newArray[a]<val) // Si suma es menor a M rompo el flujo interno y se continua con el flujo externo
+					break;
+			}
+	   } 
+	   return [0];
 }
